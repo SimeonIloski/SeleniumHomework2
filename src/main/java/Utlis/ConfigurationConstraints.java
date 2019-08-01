bd3033ec08d29d8209031bb3df6f7ebcfd04e5b2
SimeonIloski <simeoniloski@gmail.com>
2019-08-01 14:38:53 +0200

    added initial configurationConstraints implementation

---
+++ src/main/java/Utlis/ConfigurationConstraints.java
@@ -0,0 +1,13 @@
+package Utlis;
+
+public class ConfigurationConstraints {
+    public static final int MAX_RETRY_FOR_LOCATING_ELEMENT_AJAX_FACTORY = 20;
+    public static final int MAX_RETRY_FOR_LOCATING_ELEMENT = 10;
+
+
+    public static final String BASE_URL = "http://zero.webappsecurity.com";
+    public static final String USERNAME_COURSE = "username";
+    public static final String PASSWORD_COURSE = "password";
+
+
+}
